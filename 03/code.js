5fd6147fd80769a4748084f7c39449800e0a0ed5
Kim Medin <ksmedin@gmail.com>
2019-12-03 16:33:33 +0100

    day 3

--- 03/code.js
+++ 03/code.js
@@ -1,43 +1,52 @@
 const getDistanceFromCentralPortToClosestIntersection = ([firstWirePath, secondWirePath]) => {
-    console.log('first..')
     const pathOne = getWirePath(firstWirePath);
-    console.log('second..')
     const pathTwo = getWirePath(secondWirePath);
-    console.log('filtering..')
-    console.log(pathOne.length)
-    console.log(pathTwo.length)
 
-    const intersections = pathOne.filter(p1 => pathTwo.some(p2 => p1.x == p2.x && p1.y == p2.y));
-    console.log('done..')
-    console.log(intersections);
+    const parseStringToPoint = strPoint => { const s = strPoint.split(','); return { x: parseInt(s[0]), y: parseInt(s[1])}};
+    const intersections = [...pathOne.keys()]
+        .filter(p1 => pathTwo.has(p1))
+        .map(parseStringToPoint);
+
     const sum = intersections.map(i => Math.abs(i.x) + Math.abs(i.y));
-    const minDistance = Math.min(...sum);
-    return minDistance;
+    return Math.min(...sum);
+}
+
+const getFewestNumberOfCombinedStepsToAnyIntersection = ([firstWirePath, secondWirePath]) => {
+    const pathOne = getWirePath(firstWirePath);
+    const pathTwo = getWirePath(secondWirePath);
+
+    const something = [...pathOne.entries()]
+        .filter(([p1pos]) => pathTwo.has(p1pos))
+        .map(([position, steps]) => pathTwo.get(position) + steps);
+
+    return Math.min(...something);
 }
 
 const getWirePath = wire => {
-    const path = [];
-    const addPoint = (x, y) => path.push({ x, y });
+    const path = new Map();
+    const addPoint = (x, y, steps) => path.set(`${x},${y}`, steps);
 
     let x = 0;
     let y = 0;
+    let steps = 0;
     wire.split(',').forEach(w => {
         let d = w[0];
         let p = parseInt(w.substring(1));
         
         for (let i = 0; i < p; i++) {
+            ++steps;
             switch (d) {
                 case 'U':
-                    addPoint(x, ++y);
+                    addPoint(x, ++y, steps);
                     break;
                 case 'D':
-                    addPoint(x, --y);
+                    addPoint(x, --y, steps);
                     break;
                 case 'L':
-                    addPoint(--x, y);
+                    addPoint(--x, y, steps);
                     break;
                 case 'R':
-                    addPoint(++x, y);
+                    addPoint(++x, y, steps);
                     break;
                 }
         }
@@ -47,3 +56,4 @@ const getWirePath = wire => {
 }
 
 module.exports.getDistanceFromCentralPortToClosestIntersection = getDistanceFromCentralPortToClosestIntersection;
+module.exports.getFewestNumberOfCombinedStepsToAnyIntersection = getFewestNumberOfCombinedStepsToAnyIntersection;
--- 03/code.test.js
+++ 03/code.test.js
@@ -1,5 +1,5 @@
 const code = require('./code');
-//const input = require('./input');
+const input = require('./input');
 
 describe('get distance from central port to closest intersection', () => {
     it.each([
@@ -12,15 +12,26 @@ describe('get distance from central port to closest intersection', () => {
     });
 });
 
-// describe('get final answer', () => {
-//     it('Day 3 puzzle 1', () => {
-//         console.log('final');
-//         const distance = code.getDistanceFromCentralPortToClosestIntersection(input);
-//         console.log(`Day 2 Puzzle 1: ${distance}`);
-//         expect(distance).toBe(1337);
-//     });
+describe('get fewest number of combined steps to any intersection', () => {
+    it.each([
+        [['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'], 610],
+        [['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'], 410],
+    ])('the two wires should need %i steps before intersecting', (wires, expectedStepsToIntersection) => {
+        const steps = code.getFewestNumberOfCombinedStepsToAnyIntersection(wires);
+        expect(steps).toBe(expectedStepsToIntersection);
+    });
+});
+
+describe('get final answer', () => {
+    it('Day 3 puzzle 1', () => {
+        const distance = code.getDistanceFromCentralPortToClosestIntersection(input);
+        console.log(`Day 2 Puzzle 1: ${distance}`);
+        expect(distance).toBe(232);
+    });
     
-//     // it('Day 3 puzzle 2', () => {
-        
-//     // });
-// });
+    it('Day 3 puzzle 2', () => {
+        const steps = code.getFewestNumberOfCombinedStepsToAnyIntersection(input);
+        console.log(`Day 2 Puzzle 2: ${steps}`);
+        expect(steps).toBe(6084);
+    });
+});
